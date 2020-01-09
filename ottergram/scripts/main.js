b882084d083f5258f6d5ce3ad7eb9b3bd1fcb5f5
gudwlsa <gudwlsa@gmail.com>
2020-01-09 10:25:46 +0900

    Listening for the Keypress event

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -2,6 +2,7 @@ var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
   'use strict';
@@ -47,10 +48,22 @@ function hideDetails() {
   document.body.classList.add(HIDDEN_DETAIL_CLASS);
 }
 
+function addKeyPressHandler() {
+  'use strict';
+  document.body.addEventListener('keyup', function (event) {
+    event.preventDefault();
+    console.log(event.keyCode);
+    if (event.keyCode === ESC_KEY) {
+      hideDetails();
+    }
+  })
+}
+
 function initializeEvents() {
   'use strict';
   var thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
+  addKeyPressHandler();
 }
 
 initializeEvents();
