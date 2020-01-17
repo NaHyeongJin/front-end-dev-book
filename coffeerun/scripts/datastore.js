c0de53c04c0391ff4a9821a7d82d4b0f40030527
gudwlsa <gudwlsa@gmail.com>
2020-01-17 15:40:51 +0900

    A constructor's prototype

--- coffeerun/scripts/datastore.js
+++ coffeerun/scripts/datastore.js
@@ -1,10 +1,14 @@
 (function (window) {
   'use strict';
-  let App = window.App || {};
+  var App = window.App || {};
 
   function DataStore() {
-    console.log('running the DataStore function');
+    this.data = {};
   }
+
+  DataStore.prototype.add = function (key, val) {
+    this.data[key] = val;
+  };
 
   App.DataStore = DataStore;
   window.App = App;
