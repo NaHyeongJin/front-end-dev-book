17b4ce52b5171839de17d65182553c41c8a251a7
gudwlsa <gudwlsa@gmail.com>
2020-01-20 10:59:56 +0900

    Creating the Truck instance

--- coffeerun/scripts/truck.js
+++ coffeerun/scripts/truck.js
@@ -20,7 +20,7 @@
   Truck.prototype.printOrders = function () {
     var customerIdArray = Object.keys(this.db.getAll());
 
-    console.log('Truck #' + this.truckId + 'has pending orders:');
+    console.log('Truck #' + this.truckId + ' has pending orders:');
     customerIdArray.forEach(function (id) {
       console.log(this.db.get(id));
     }.bind(this));
