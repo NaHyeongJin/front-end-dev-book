40907a90f414a5aa669e163872f415f8a40008bc
gudwlsa <gudwlsa@gmail.com>
2020-01-20 10:27:53 +0900

    Removing orders

--- coffeerun/scripts/truck.js
+++ coffeerun/scripts/truck.js
@@ -12,6 +12,11 @@
     this.db.add(order.emailAddress, order);
   };
 
+  Truck.prototype.deliverOrder = function (customerId) {
+    console.log('Delivering order for ' + customerId);
+    this.db.remove(customerId);
+  };
+
   App.Truck = Truck;
   window.App = App;
 })(window);
