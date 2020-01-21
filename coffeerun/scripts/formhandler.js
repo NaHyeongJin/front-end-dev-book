ea70af4bd64d3df3e4c29e38ebf3e7da7ae96cd9
gudwlsa <gudwlsa@gmail.com>
2020-01-21 11:08:16 +0900

    Extracting the data

--- coffeerun/scripts/formhandler.js
+++ coffeerun/scripts/formhandler.js
@@ -3,7 +3,7 @@
   var App = window.App || {};
   var $ = window.jQuery;
 
-  function FormHandler() {
+  function FormHandler(selector) {
     if (!selector) {
       throw new Error('No selector provided');
     }
@@ -18,6 +18,13 @@
     console.log('Setting submit handler for form');
     this.$formElement.on('submit', function (event) {
       event.preventDefault();
+
+      var data = {};
+      $(this).serializeArray().forEach(function (item) {
+        data[item.name] = item.value;
+        console.log(item.name + ' is ' + item.value);
+      })
+      console.log(data);
     })
   };
 
