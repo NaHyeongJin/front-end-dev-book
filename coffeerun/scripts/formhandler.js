a18b77fe770d0c7340d2e1b94c0e7f79a487ff48
gudwlsa <gudwlsa@gmail.com>
2020-01-21 10:46:31 +0900

    Creating the FormHandler Module and adding jQuery

---
+++ coffeerun/scripts/formhandler.js
@@ -0,0 +1,11 @@
+(function (window) {
+  'use strict';
+  var App = window.App || {};
+
+  function FormHandler() {
+    // Code will go here
+  }
+
+  App.FormHandler = FormHandler;
+  window.App = App;
+})(window);
