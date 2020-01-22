c25301f7dda8e6c14b584cadfed67e4bfab99f7a
gudwlsa <gudwlsa@gmail.com>
2020-01-22 16:21:39 +0900

    Creating the CheckList.prototype.removeRow method

--- coffeerun/scripts/checklist.js
+++ coffeerun/scripts/checklist.js
@@ -23,6 +23,11 @@
     this.$element.append(rowElement.$element);
   };
 
+  CheckList.prototype.removeRow = function (email) {
+    this.$element.find('[value="' + email + '"]').closest('[data-coffee-order="checkbox"]').remove();
+
+  };
+
   function Row(coffeeOrder) {
     var $div = $('<div></div>', {
       'data-coffee-order': 'checkbox',
